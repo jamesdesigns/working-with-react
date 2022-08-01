4d91800118b373c7bda8eaf0348abc3b5a497650
James Hooper <jameshooperdesigns@gmail.com>
2022-08-01 15:29:24 -0600

    Add useState to project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [emotion, setEmotion] = useState("happy");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`);    
+  }, [emotion])
+
   return (
     <div className="App">
-      <h1>Hello World.</h1>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
     </div>
   );
 }
